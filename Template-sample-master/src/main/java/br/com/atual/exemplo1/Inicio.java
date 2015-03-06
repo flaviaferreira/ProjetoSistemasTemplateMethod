99e9227437766a5cdbba22869e1c9af7cbfe7d01
flaviaferreira <flaviahferreirah@gmail.com>
2015-03-06 20:58:30 -0300

    Enviando trabalho sobre Template Method.

---
+++ Template-sample-master/src/main/java/br/com/atual/exemplo1/Inicio.java
@@ -0,0 +1,36 @@
+package br.com.atual.exemplo1;
+
+/**
+ * Created by Ton on 04/03/2015.
+ */
+public class Inicio {
+
+
+    public static void main(String[] args) {
+        System.out.println("Teste salvando");
+
+        DaoPessoaFisica daoFisica = new DaoPessoaFisica();
+        PessoaFisica pf = new PessoaFisica();
+        pf.setId(0);
+        pf.setNome("Ana");
+        pf.setCpf("04251217985");
+        daoFisica.salvar(pf);
+        
+        DaoPessoaJuridica daoJuridica = new DaoPessoaJuridica();
+        PessoaJuridica pj = new PessoaJuridica();
+        pj.setNome("Empresa Tal");
+        pj.setCnpj("11232564000111");
+        daoJuridica.salvar(pj);
+
+        DaoCliente daoCliente = new DaoCliente();
+        Cliente cliente = new Cliente();
+        cliente.setId(0);
+        cliente.setNome("Cliente legal");
+        cliente.setNumeroCliente(123);
+
+        daoCliente.salvar(cliente);
+
+
+    }
+
+}
